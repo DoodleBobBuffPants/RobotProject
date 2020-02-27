ad51c7cff910ed9e6d524d4bd2367668a432751e
DoodleBobBuffPants <ajayseanx@gmail.com>
2020-02-27 20:51:25 +0000

    Actually include all of Ben's changes

--- project/ros/velocity_controller/init_formations.py
+++ project/ros/velocity_controller/init_formations.py
@@ -2,28 +2,29 @@
 import numpy as np
 
 SPACING_DIST = 0.3
-LINE = np.array([[-2*SPACING_DIST,0], 
+LINE = np.array([[2*SPACING_DIST,0], 
         [SPACING_DIST, 0], 
         [0, 0], 
-        [SPACING_DIST, 0], 
-        [2*SPACING_DIST, 0]])
+        [-1*SPACING_DIST, 0], 
+        [-2*SPACING_DIST, 0]])
 
 COLUMN =   np.array([[0, 2*SPACING_DIST], 
             [0, SPACING_DIST], 
             [0, 0], 
-            [0,-SPACING_DIST], 
+            [0,-1*SPACING_DIST], 
             [0, -2*SPACING_DIST]])
 
+#TODO: update diamond to be top to bottom, right to left
 DIAMOND =  np.array([[-SPACING_DIST, 0], 
             [0, SPACING_DIST], 
             [0, 0], 
             [0,-SPACING_DIST], 
             [SPACING_DIST, 0]])
 
-WEDGE =  np.array([[-2*SPACING_DIST, -SPACING_DIST], 
-            [-SPACING_DIST, 0], 
-            [0, SPACING_DIST], 
+WEDGE =  np.array([[2*SPACING_DIST, -SPACING_DIST], 
             [SPACING_DIST, 0], 
-            [2*SPACING_DIST, -SPACING_DIST]])
+            [0, SPACING_DIST], 
+            [-1*SPACING_DIST, 0], 
+            [-2*SPACING_DIST, -SPACING_DIST]])
 
 FORMATION = LINE
