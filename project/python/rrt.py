5263191e4a26e4c5232e613395c722df07bcd427
BenjaminPhi5 <bp413@cam.ac.uk>
2020-02-27 15:38:06 +0000

    back to previous rrt

--- project/python/rrt.py
+++ project/python/rrt.py
@@ -10,6 +10,7 @@ import os
 import re
 import scipy.signal
 import yaml
+import random
 
 
 # Constants used for indexing.
@@ -29,48 +30,22 @@ MAX_ITERATIONS = 500
 
 
 def sample_random_position(occupancy_grid):
-  position = np.zeros(2, dtype=np.float32)
 
   # MISSING: Sample a valid random position (do not sample the yaw).
   # The corresponding cell must be free in the occupancy grid.
 
-  # This is implementing the first primitive of the RRT algorithm: sample configurations in free C-space.
-  # C free = C \ C obs, the occupancy_grid function is giving us a nice 
+  x = random.uniform(-2, 2)
+  y = random.uniform(-2, 2)
+  while not occupancy_grid.is_free(np.array([x, y])):
+  	x = random.uniform(-2, 2)
+  	y = random.uniform(-2, 2)
 
-  # sample function not including yaw, capped at dimensions of the arena
-  sample_pos = lambda: np.random.uniform(low=-2, high=3.5, size=2)
-
-  position = sample_pos()
-  while not is_valid(position, occupancy_grid):
-    position = sample_pos()
-
-  return position
-
- # function to check that this position is valid for the robot to be in (check points at the robot's circumpherence)
-def is_valid(position, occupancy_grid):
-  # function for checking if a position is valid
-  pos_is_valid = lambda pos: occupancy_grid.is_free(pos) and not occupancy_grid.is_occupied(pos)
-
-  # check 4 corners (and the centre itself) (for now), this can be extended
-  r = ROBOT_RADIUS
-  rob_radius_points = [[0,0], [0, r], [0, -r], [r, 0], [-r, 0]]
-  for radius_marker in rob_radius_points:
-    # get radius position
-    radius_pos = position + radius_marker
-    # check if it is valid
-    if not pos_is_valid(radius_pos):
-      return False
-    
-    # no obstacles found at this position, return true
-    return True
+  return np.array([x, y])
 
 
 def adjust_pose(node, final_position, occupancy_grid):
   final_pose = node.pose.copy()
   final_pose[:2] = final_position
-  # final node is constructed using the final_position x and y components
-  # the aim of this task is to find out if it is valid, and if so compute a corresponding valid YAW
-  final_node = Node(final_pose)
 
   # MISSING: Check whether there exists a simple path that links node.pose
   # to final_position. This function needs to return a new node that has
@@ -80,203 +55,24 @@ def adjust_pose(node, final_position, occupancy_grid):
   # Assume that the robot always goes forward.
   # Feel free to use the find_circle() function below.
 
-  # use the current node and the next node, check if there is a circle
-  # if no arc return None
-  # can the circle function return more than one value... who knowns
-  # can you not I do not
-
-  # get the circle that connects the robot to the result node
-  centre, radius = find_circle(node, final_node)
-
-  # theta of the robot from the starting node YAW
-  theta_robot = node.pose[YAW]
-
-  # compute if theta direction is valid given the circle
-  # first get the vector from the robot to the centre
-  radius_vec = - node.pose[:2] + centre
-
-  # get radius angle
-  theta_rad = get_angle_of_vector(radius_vec)
-  
-  # rotate the vector by -pi/2 to get the tangent of the circle
-  p_div_2 = np.pi / 2.
-  tangent_vec = np.matmul([
-    [np.cos(p_div_2), np.sin(p_div_2)],
-    [-np.sin(p_div_2), np.cos(p_div_2)]
-  ],
-   radius_vec)
-
-  # check if the robot's  yaw is within pi/4 rads either side of the radius angle
-  # if not, the arc is not valid, return None
-  pi_div2 = np.pi / 2
-  # the robot must be within this range otherwise it would have to modify w as it was moving between points
-  # to smoothly curve between them, here we are just assuming a constant w between positions I think.
-  if theta_rad - pi_div2 <= theta_robot <= theta_rad + pi_div2:
-    # the angle is valid, compute the new yaw
-
-    # see paper write up, the new yaw is 2 * theta_radius - theta_robot
-    # if (thetar - theta robot) > 0, its going anticlockwise, yaw = theta r + (theta r - theta robot) = 2 theta r - theta robot
-    # if (thetar - theta robot) < 0, its going clockwise, yaw = theta r - (theta robot - theta r) = 2 theta r - theta robot
-    final_pose[YAW] = (2 * theta_rad) - theta_robot
-
-    # now we need to sample this arc to check that there are no collisions
-    # make the image much bigger with more obstacles so that in the report, you can show that your rrt really does go around the obstacles.
-
-    # get the new centre of the circle given the arc that the robot actually will traverse
-    centre = get_new_circle_centre(node.pose, final_pose)
-
-    dist_between_points = lambda a, b: np.sqrt(np.square(a[X]-b[X]) + np.square(a[Y]-b[Y]))
-    # next get the angle that subtends the arc of the circle
-    #print(dist_between_points(centre, final_position))
-    #print(dist_between_points(node.position, centre))
-    assert dist_between_points(centre, final_position) - dist_between_points(node.position, centre) < 0.001
-
-    # opp = distance between final position and starting position
-    # hype = distance between centre and either of the poses
-    opp = dist_between_points(final_position, node.position) / 2.
-    hyp = dist_between_points(final_position, centre)
-
-    # angle that subtends the arc is two times the angle in the triangle defined using opp and hyp
-    phi = np.arcsin(opp/hyp) * 2.
-    #print("ratio:", opp/hyp)
-    #print("phi: ", phi)
-    # if np.isnan(phi):
-    #   phi = np.pi
-
-    # step size at which to check points on the line between start and finish for occupancy
-    step_size = 0.1
-
-    # check for straight line case (infinte new circle radius):
-    straight_tolerance = 0.01
-    if abs(theta_rad - theta_robot) < straight_tolerance:
-      #print("Picked straight line")
-      # if there is a straight line between the points, check validity on points on the line between them
-      distance = opp * 2.
-      vector = final_position - node.position
-      steps = distance / step_size
-      for step in range(0, int(round(steps+1))):
-        step_position = node.position + step * step_size * vector
-        if not is_valid(step_position, occupancy_grid):
-          return None
-    
-    elif dist_between_points(final_position, node.position) < 2. * ROBOT_RADIUS:
-      # tiny circle, just compute is valid at the destination
-      if not is_valid(final_position, occupancy_grid):
-        return None
-    
-    else: # we are going in a clockwise or anticlockwise circle
-      # compute new arc length
-      new_radius_length = hyp
-      arc_length = new_radius_length * phi
-      steps = arc_length / step_size
-      step_angle = phi / steps
-
-      #print("radisu length: ", new_radius_length)
-      #print("arc length: ", arc_length)
-      #print("steps: ", steps)
-
-      points = []
-
-      # offset is pi/2 if going clockwise, or -pi/2 if going anticlockwise
-      # using the **origianl** circle's theta radius
-      offset = -np.pi/2. if (theta_rad - theta_robot) > 0 else +np.pi/2. # theta_rad-theta_tobot is < 0 for anticlockwise
-      direction = 1 if (theta_rad - theta_robot) > 0 else -1. # theta_rad-theta_tobot is < 0 for anticlockwise
-  
-      # step along the arc, looking for collisions
-      for step in range(0, int(round(steps+1))):
-        angle = node.pose[YAW] + offset + (step * step_angle * direction)
-        
-        # compute x, y, coords
-        x = centre[X] + new_radius_length * np.cos(angle)
-        y = centre[Y] + new_radius_length * np.sin(angle)
-
-        points.append([x, y])
-
-        # compute the position is okay in the map
-        # if it is not, return False
-        if not is_valid(np.array([x,y]), occupancy_grid):
-          return None
-
-      # check the destination
-      if not is_valid(final_position, occupancy_grid):
-        return None
-
-      #print("#######################")
-      #print("a: ", node.position)
-      #print("b: ", final_position)
-      #print("step points: ", points)
-      #print("#######################")
-
+  vec = final_position - node.position
+  vec = vec / np.linalg.norm(vec)
+  theta = np.arccos(np.dot(vec, node.direction))
+  if np.cross(node.direction, vec).item() > 0.:
+  	final_pose[YAW] = node.yaw + 2.*theta
   else:
-    return None
-
-  #print("robot theta:   ", theta_robot)
-  #print("tangent theta: ", theta_rad)
-  #print("------------------------------")
-  #print("------------------------------")
+  	final_pose[YAW] = node.yaw - 2.*theta
 
   final_node = Node(final_pose)
+  center, radius = find_circle(node, final_node)
+
+  for x in np.arange(-2, 2+occupancy_grid.resolution, occupancy_grid.resolution):
+  	for y in np.arange(-2, 2+occupancy_grid.resolution, occupancy_grid.resolution):
+  		if (x-center[X])**2 + (y-center[Y])**2 == radius**2:
+  			if not occupancy_grid.is_free(np.array([x, y])):
+  				return None
+
   return final_node
-
-def get_new_circle_centre(pose_a, pose_b):
-  # get gradients of radius lines from points a and b, by shifting tangent gradient by pi/4 degrees
-
-  m_a = np.tan(pose_a[YAW] + (np.pi/2.))
-  m_b = np.tan(pose_b[YAW] + (np.pi/2.))
-  #m_a = np.tan(pose_a[YAW])
-  #m_b = np.tan(pose_b[YAW])
-
-  # compute constant c components using the pose coords and the gradients
-  # y = mx + c, => c = y - mx
-  c_a = pose_a[Y] - m_a * pose_a[X]
-  c_b = pose_b[Y] - m_b * pose_b[X]
-
-  # compute the centre of the new circle
-  centre = np.array([
-    (c_b - c_a) / (m_a - m_b),
-    (c_a - m_a * c_b / m_b) / (1 - m_a / m_b)
-  ])
-
-  return centre
-
-
-def get_angle_of_vector(vector):
-  angle = np.arctan(vector[Y]/vector[X])
-  # make sure our angle is positive only
-  if vector[X] < 0:
-    angle = np.pi + angle
-  angle = (angle + (np.pi * 2)) % (np.pi * 2)
-
-  return angle
-
-# # now compute the angle of the tangent vector
-#   theta_vec = np.arctan(vec[Y]/vec[X])
-#   pi2 = np.pi * 2
-
-#   # account for arctan function
-#   if vec[X] < 0:
-#     theta_vec = np.pi + theta_vec
-#   theta_vec = (theta_vec + pi2) % pi2
-
-#   print("robot theta:   ", theta_rob)
-#   print("tangent theta: ", theta_vec)
-
-#   # now check if robot yaw or robot yaw + pi is within tolerance of the theta_vec
-#   tolerance = 2.
-#   if abs(theta_vec - theta_rob) < tolerance or abs(-np.pi + theta_vec - theta_rob) < tolerance:
-#     # for now don't do any path checking, just return theta_rob + 180 and return
-#     theta_rob = (theta_vec + np.pi) % pi2
-#     final_pose[YAW] = theta_rob
-
-#   else:
-#     # robot is not facing in an approporiate direction to traverse this circle, return None
-#     return None
-#     #pass
-  
-
-#   final_node = Node(final_pose)
-#   return final_node
-
 
 
 # Defines an occupancy grid.
@@ -316,7 +112,6 @@ class OccupancyGrid(object):
     plt.set_cmap('gray_r')
 
   def get_index(self, position):
-    # computes an index into the values table for that position.
     idx = ((position - self._origin) / self._resolution).astype(np.int32)
     if len(idx.shape) == 2:
       idx[:, 0] = np.clip(idx[:, 0], 0, self._values.shape[0] - 1)
@@ -396,8 +191,7 @@ def rrt(start_pose, goal_position, occupancy_grid):
   for _ in range(MAX_ITERATIONS): 
     position = sample_random_position(occupancy_grid)
     # With a random chance, draw the goal position.
-    if np.random.rand() < .2:
-    #if np.random.rand() < .05:
+    if np.random.rand() < .05:
       position = goal_position
     # Find closest node in graph.
     # In practice, one uses an efficient spatial structure (e.g., quadtree).
