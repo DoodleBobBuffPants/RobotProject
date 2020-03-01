5c8aaa2a3b933ddedb4903bbbaf9fb4f0a8d4d89
DoodleBobBuffPants <ajayseanx@gmail.com>
2020-03-01 15:02:12 +0000

    Add initial yaw param

--- project/ros/velocity_controller/init_formations.py
+++ project/ros/velocity_controller/init_formations.py
@@ -10,6 +10,7 @@ SPACING_DIST = 0.3
 # In leader follower, only the followers need be defined in the formation.
 
 LEADER_ID = 2
+INITIAL_YAW = 1.571
 LINE = np.array([[2*SPACING_DIST,0], 
 				 [SPACING_DIST, 0],
 				 [-SPACING_DIST, 0], 
@@ -27,6 +28,7 @@ COLUMN = np.array([[0, 2*SPACING_DIST],
 #               3
 
 # LEADER_ID = 0
+# INITIAL_YAW = 1.571
 DIAMOND =  np.array([[-SPACING_DIST, -SPACING_DIST], 
 					 [0, -SPACING_DIST], 
 					 [0, -2.*SPACING_DIST], 
