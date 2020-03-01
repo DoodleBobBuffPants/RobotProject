8cd99395a6249ccc7c76244fdc1ef4f485ad63f5
DoodleBobBuffPants <ajayseanx@gmail.com>
2020-03-01 02:53:20 +0000

    Fix formations

--- project/ros/velocity_controller/init_formations.py
+++ project/ros/velocity_controller/init_formations.py
@@ -2,7 +2,6 @@
 import numpy as np
 
 SPACING_DIST = 0.3
-LEADER_ID = 0
 
 # NOTE:
 # due to changing to leader follower, the formations need some adjusting (of the order of positions)
@@ -10,34 +9,33 @@ LEADER_ID = 0
 # NOTE:
 # In leader follower, only the followers need be defined in the formation.
 
-# LINE = np.array([[2*SPACING_DIST,0], 
-#         [SPACING_DIST, 0], 
-#         [0, 0], 
-#         [-1*SPACING_DIST, 0], 
-#         [-2*SPACING_DIST, 0]])
+# LEADER_ID = 2
+LINE = np.array([[2*SPACING_DIST,0], 
+				 [SPACING_DIST, 0],
+				 [-SPACING_DIST, 0], 
+				 [-2*SPACING_DIST, 0]])
 
-# COLUMN =   np.array([[0, 2*SPACING_DIST], 
-#             [0, SPACING_DIST], 
-#             [0, 0], 
-#             [0,-1*SPACING_DIST], 
-#             [0, -2*SPACING_DIST]])
-
+# LEADER_ID = 2
+COLUMN = np.array([[0, 2*SPACING_DIST], 
+            	   [0, SPACING_DIST],
+            	   [0,-*SPACING_DIST], 
+            	   [0, -2*SPACING_DIST]])
 
 # DIAMOND
-#         LEADER (0) not defined
+#       LEADER (0) not defined
 #       1       2       4
 #               3
-#
-#
+
+LEADER_ID = 0
 DIAMOND =  np.array([[-SPACING_DIST, -SPACING_DIST], 
 					 [0, -SPACING_DIST], 
 					 [0, -2.*SPACING_DIST], 
 					 [SPACING_DIST, -SPACING_DIST]])
 
-# WEDGE =  np.array([[2*SPACING_DIST, -SPACING_DIST], 
-#             [SPACING_DIST, 0], 
-#             [0, SPACING_DIST], 
-#             [-1*SPACING_DIST, 0], 
-#             [-2*SPACING_DIST, -SPACING_DIST]])
+# LEADER_ID = 2
+WEDGE = np.array([[2*SPACING_DIST, -SPACING_DIST], 
+				  [SPACING_DIST, 0],
+				  [-*SPACING_DIST, 0], 
+				  [-2*SPACING_DIST, -SPACING_DIST]])
 
 FORMATION = DIAMOND
