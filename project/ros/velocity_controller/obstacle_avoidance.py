b80f39dabbf3ee25a6292abe9c00a53e05f8b637
DoodleBobBuffPants <ajayseanx@gmail.com>
2020-03-07 12:52:05 +0000

    better avoiding

--- project/ros/velocity_controller/obstacle_avoidance.py
+++ project/ros/velocity_controller/obstacle_avoidance.py
@@ -17,9 +17,9 @@ def rule_based(front, front_left, front_right, left, right):
   if sens_inp[0] < np.tanh(.3):
     u = -1.
     if sens_inp[1] < sens_inp[2]:
-      w = -4.*np.pi
+      w =  3.*np.pi
     else:
-      w =  4.*np.pi
+      w = -3.*np.pi
     return u, w
 
   return u, w
