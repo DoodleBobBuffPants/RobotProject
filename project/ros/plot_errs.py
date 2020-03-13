undo erroneous change, add units to axis

project/ros/plot_errs.py
@@ -14,13 +14,14 @@ with open(os.path.expanduser("~/.ros/errors.txt"), "r") as f:
       errors.append(v)
 
 sampled_errs = [errors[i][::20] for i in range(len(errors))]
+min_len = min([len(err) for err in sampled_errs])
 
 x = np.arange(min_len)
 for i in range(len(sampled_errs)):
-  plt.plot(x, sampled_errs[i])
+  plt.plot(x, sampled_errs[i][:min_len])
 
-plt.xlabel('Time')
-plt.ylabel('Error')
+plt.xlabel('Time (s)')
+plt.ylabel('Error (m)')
 plt.legend(robot_names)
 
 os.remove(os.path.expanduser("~/.ros/errors.txt"))